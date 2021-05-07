357401f2213d9ba85bd37b48d472cfb3ce60f407
Tarunverma504 <vermatarun4305@gmail.com>
2021-05-07 08:59:21 +0530

    updated Audio player

--- src/component/Artist.js
+++ src/component/Artist.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/component/Artist.js
 import React,{Component,useState} from 'react';
 import "../../node_modules/jquery/dist/jquery.min.js";
 import "../../node_modules/bootstrap/js/src/collapse";
@@ -26,33 +25,4 @@ export default function Artist(props){
         </div>
         </>
         )
-=======
-import React,{Component,useState} from 'react';
-import "../node_modules/jquery/dist/jquery.min.js";
-import "../node_modules/bootstrap/js/src/collapse";
-import "../node_modules/bootstrap/dist/css/bootstrap.css" ;
-import "./Artist.css";
-var arr=[];
-export default function Artist(props){
-    for (var i=0;i<8;i++)
-    {
-        arr.push(<div className=" col justify-content-between ">
-        <img className="rounded-circle resizeimg " src={props.src} width={150} height={150}/>
-        <br/>
-        <p className="px-2 ml-4" style={{color:'white'}}>Honeypreet</p>
-    </div>)
-    }
-    return(
-        <>
-        <div className=" .container-fluid">
-            <div className="container ">
-                <h2 style={{color:'white'}}>Artists</h2>
-                <div className="row row-cols-md-4 mt-4  text-align-center ">
-                {arr}          
-                </div>
-            </div>
-        </div>
-        </>
-        )
->>>>>>> 17238e99748764b4d205712d838d4f1deed87846:src/Artist.js
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
 import App from './component/App';
 import Artist from "./component/Artist";
 import Player from "./component/Player";
 import "./style/App.css";
-=======
-import App from './App';
-import Artist from "./Artist";
-import Player from "./Player";
->>>>>>> 17238e99748764b4d205712d838d4f1deed87846
 ReactDOM.render(
- <Player/>
+ <App/>
  ,  document.getElementById('root')
 );
 
